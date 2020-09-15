02cd9df9308e7cc8ea96170bd65727a59f16c4ce
Apoorv Goyal <apoorvelous@gmail.com>
2020-09-15 23:01:20 +0530

    fixed

--- screens/Camera.js
+++ screens/Camera.js
@@ -1,107 +1,7 @@
-// import React from "react";
-// import { View, Text, TouchableOpacity } from "react-native";
-// import { Camera } from "expo-camera";
-// import * as Permissions from "expo-permissions";
-// import {
-//   FontAwesome,
-//   Ionicons,
-//   MaterialCommunityIcons,
-// } from "@expo/vector-icons";
-//
-// import styles from "./styles";
-//
-// export default class CameraPage extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       hasCameraPermission: null,
-//     };
-//     this.camera = null;
-//   }
-//   async componentDidMount() {
-//     const camera = await Permissions.askAsync(Permissions.CAMERA);
-//     const audio = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
-//     const hasCameraPermission =
-//       camera.status === "granted" && audio.status === "granted";
-//     this.setState({ hasCameraPermission });
-//   }
-//
-//   uploadImage = async (url) => {
-//     //creatig the form data
-//     const fileToUpload =
-//       "https://3qeqpr26caki16dnhd19sv6by6v-wpengine.netdna-ssl.com/wp-content/uploads/2019/02/sample_image.png";
-//     const data = new FormData();
-//     data.append("digit", fileToUpload);
-//     let res = await fetch("https://5ffb373a1726.ngrok.io/predict-digit", {
-//       method: "POST",
-//       body: data,
-//     })
-//       .then((response) => response.json())
-//       .then((result) => {
-//         console.log("Success:", result);
-//       })
-//       .catch((error) => {
-//         console.error("Error:", error);
-//       });
-//
-//     // // if (responseJson.status == 1) {
-//     //   console.log("Upload Successful");
-//     // }
-//   };
-//
-//   takePicture = async () => {
-//     if (this.camera) {
-//       let photo = await this.camera.takePictureAsync();
-//       var image = photo.uri;
-//       this.uploadImage(image);
-//     }
-//   };
-//
-//   render() {
-//     const { hasCameraPermission } = this.state;
-//
-//     if (hasCameraPermission === null) {
-//       return <View />;
-//     } else if (hasCameraPermission === false) {
-//       return <Text>Access to camera has been denied.</Text>;
-//     } else {
-//       return (
-//         <View style={{ flex: 1, alignItems: "flex-end" }}>
-//           <Camera
-//             style={styles.preview}
-//             ref={(ref) => {
-//               this.camera = ref;
-//             }}
-//           />
-//
-//           <TouchableOpacity
-//             style={{
-//               alignSelf: "flex-end",
-//               alignItems: "center",
-//               backgroundColor: "transparent",
-//             }}
-//             onPress={() => this.takePicture()}
-//           >
-//             <FontAwesome
-//               name="camera"
-//               style={{ color: "#fff", fontSize: 40 }}
-//             />
-//           </TouchableOpacity>
-//         </View>
-//       );
-//     }
-//   }
-// }
-
 import * as React from "react";
 import { Button, Image, View, Platform } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import Constants from "expo-constants";
 import * as Permissions from "expo-permissions";
-import RNFetchBlob from "rn-fetch-blob";
-import axios from "axios";
-
-const Blob = RNFetchBlob.polyfill.Blob;
 
 export default class ImagePickerExample extends React.Component {
   state = {
@@ -137,22 +37,17 @@ export default class ImagePickerExample extends React.Component {
     }
   };
 
-  uploadImage = async (response) => {
-    //     //creatig the form data
+  uploadImage = async (uri) => {
     const data = new FormData();
-    const imageFile = RNFetchBlob.wrap(path);
-    let uploadBlob;
-    await Blob.buid(imageFile, { type: "image/jpg;" }).then((imageBlob) => {
-      uploadBlob = imageBlob;
-    });
-    // const base64 = await this.blobToBase64(response);
+    let filename = uri.split("/")[uri.split("/").length - 1]
+    let type = `image/${uri.split('.')[uri.split('.').length - 1]}`
     const fileToUpload = {
-      uri: response,
+      uri: uri,
       name: filename,
       type: type,
     };
-    data.append("digit", uploadBlob);
-    fetch("https://f1abd62234a3.ngrok.io/predict-digit", {
+    data.append("digit", fileToUpload);
+    fetch("https://da16e588d3bf.ngrok.io/predict-digit", {
       method: "POST",
       body: data,
       headers: {
@@ -166,10 +61,6 @@ export default class ImagePickerExample extends React.Component {
       .catch((error) => {
         console.error("Error:", error);
       });
-
-    // // if (responseJson.status == 1) {
-    //   console.log("Upload Successful");
-    // }
   };
 
   _pickImage = async () => {
@@ -182,31 +73,11 @@ export default class ImagePickerExample extends React.Component {
       });
       if (!result.cancelled) {
         this.setState({ image: result.data });
+        console.log(result.uri)
         this.uploadImage(result.uri);
       }
     } catch (E) {
       console.log(E);
     }
   };
-  // try {
-  //   const res = await DocumentPicker.pick({
-  //     //Provide which type of file you want user to pick
-  //     type: [DocumentPicker.types.images],
-  //   });
-  //   //Printing the log realted to the file
-  //   console.log("res : " + JSON.stringify(res));
-  //   //Setting the state to show single file attributes
-  //   uploadImage(res);
-  // } catch (err) {
-  //   //Handling any exception (If any)
-  //   if (DocumentPicker.isCancel(err)) {
-  //     //If user canceled the document selection
-  //     alert("Canceled from single doc picker");
-  //   } else {
-  //     //For Unknown Error
-  //     alert("Unknown Error: " + JSON.stringify(err));
-  //     throw err;
-  //   }
-  // }
-  // };
 }
